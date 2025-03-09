37eef72b7d4bbed65b78f35d0d2c47903f49aa2b
mathebrard <mathebrard47@gmail.com>
2025-03-09 15:24:35 +0100

    ajout texte agence

--- src/app/features/projet/pages/projet/projet.component.ts
+++ src/app/features/projet/pages/projet/projet.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-projet',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './projet.component.html',
-  styleUrl: './projet.component.scss'
+  styleUrl: './projet.component.scss',
 })
-export class ProjetComponent {
-
-}
+export class ProjetComponent {}
